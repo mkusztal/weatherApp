bd5dc40c2d867244896603407661eb25a2c9987b
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-05-27 07:50:25 +0300

    Add ErrorBox

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,37 +1,45 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 import { useCallback, useState } from 'react';
 
 const WeatherBox = (props) => {
   const [cityWeather, setCityWeather] = useState('');
+  const [pending, setPending] = useState(false);
+  const [detectedError, setDetectedError] = useState(false);
 
   const handleCityChange = useCallback((city) => {
     const cityName = city;
-
+    setPending(true);
+    setDetectedError(false);
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6d0bccc9ad8a3faed305b98156d2843b&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
 
-        setCityWeather(weatherData);
-
-        console.log(weatherData);
-      });
+          setPending(false);
+          setCityWeather(weatherData);
+        });
+      } else {
+        setDetectedError(true);
+      }
+    });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary {...cityWeather} />
-      <Loader />
+      {cityWeather && !pending && <WeatherSummary {...cityWeather} />}
+      {pending && !detectedError && <Loader />}
+      {detectedError && <ErrorBox />}
     </section>
   );
 };
